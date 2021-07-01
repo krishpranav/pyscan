c0a7b2d81767ea2eb9117d65906e9c7c39a31fbf
krishpranav <krisna.pranav@gmail.com>
2021-07-01 13:44:04 +0530

    [+] imports & colors [+]

---
+++ modules/gameserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# imports
+import socket
+import datetime
+import sys
+import ipaddress
+import threading
+import os 
+
+# colors
+BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[1;94m', '\033[1;91m', '\33[1;97m', '\33[1;93m', '\033[1;35m', '\033[1;32m', '\033[0m'
+
