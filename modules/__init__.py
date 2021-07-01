187e9705293f1a59ec701fe731985d2d8ab52469
krishpranav <krisna.pranav@gmail.com>
2021-07-01 14:02:18 +0530

    [+] initialize [+]

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# imports
+from os.path import dirname, basename, isfile
+import glob
+
+modules = glob.glob(dirname(__file__)+"/*.py")
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
