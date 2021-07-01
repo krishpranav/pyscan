2bfa62b6b9d23a0cc46034a38b681cdfd1056d79
krishpranav <krisna.pranav@gmail.com>
2021-07-01 14:00:23 +0530

    [+] template [+]

---
+++ modules/template.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# core options
+def coreOptions():
+    options = [["testvar1", "testvar1 description", ""], ["testvar2", "testvar2 description", "testvar2 pre-set value"], ["var3", "var3 description", ""]]
+    return options
