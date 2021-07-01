47d1a8a6fcb072217a64eb10db85160a36565058
krishpranav <krisna.pranav@gmail.com>
2021-07-01 13:44:35 +0530

    [+] init [+]

--- modules/gameserver.py
+++ modules/gameserver.py
@@ -11,3 +11,11 @@ import os
 # colors
 BLUE, RED, WHITE, YELLOW, MAGENTA, GREEN, END = '\33[1;94m', '\033[1;91m', '\33[1;97m', '\33[1;93m', '\033[1;35m', '\033[1;32m', '\033[0m'
 
+
+class ThreadManager(object):
+    i = 0
+
+    def __init__(self, ipList):
+        self.allIps = ipList
+        self.size = len(ipList)
+        
